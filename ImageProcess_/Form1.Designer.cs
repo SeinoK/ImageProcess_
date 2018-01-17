8488267d5a1990e317805e488f9ae02ceb0889da
SeinoK <whoroad@live.cn>
2018-01-18 00:08:13 +0800

    test

---
+++ ImageProcess_/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace ImageProcess_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Open = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.Close = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pixel = new System.Windows.Forms.Button();
+            this.memory = new System.Windows.Forms.Button();
+            this.pointer = new System.Windows.Forms.Button();
+            this.normalConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(45, 43);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(75, 23);
+            this.Open.TabIndex = 0;
+            this.Open.Text = "Open Image";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(45, 96);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 1;
+            this.Save.Text = "Save Image";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Close
+            // 
+            this.Close.Location = new System.Drawing.Point(45, 153);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(75, 23);
+            this.Close.TabIndex = 2;
+            this.Close.Text = "Close Image";
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 624);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "File name: ";
+            // 
+            // pixel
+            // 
+            this.pixel.Location = new System.Drawing.Point(45, 208);
+            this.pixel.Name = "pixel";
+            this.pixel.Size = new System.Drawing.Size(75, 23);
+            this.pixel.TabIndex = 4;
+            this.pixel.Text = "Get Pixel";
+            this.pixel.UseVisualStyleBackColor = true;
+            this.pixel.Click += new System.EventHandler(this.pixel_Click);
+            // 
+            // memory
+            // 
+            this.memory.Location = new System.Drawing.Point(45, 259);
+            this.memory.Name = "memory";
+            this.memory.Size = new System.Drawing.Size(75, 23);
+            this.memory.TabIndex = 5;
+            this.memory.Text = "Memory Get";
+            this.memory.UseVisualStyleBackColor = true;
+            this.memory.Click += new System.EventHandler(this.memory_Click);
+            // 
+            // pointer
+            // 
+            this.pointer.Location = new System.Drawing.Point(45, 315);
+            this.pointer.Name = "pointer";
+            this.pointer.Size = new System.Drawing.Size(75, 23);
+            this.pointer.TabIndex = 6;
+            this.pointer.Text = "Pointer Get";
+            this.pointer.UseVisualStyleBackColor = true;
+            this.pointer.Click += new System.EventHandler(this.pointer_Click);
+            // 
+            // normalConvert
+            // 
+            this.normalConvert.Location = new System.Drawing.Point(45, 367);
+            this.normalConvert.Name = "normalConvert";
+            this.normalConvert.Size = new System.Drawing.Size(75, 23);
+            this.normalConvert.TabIndex = 7;
+            this.normalConvert.Text = "Normal";
+            this.normalConvert.UseVisualStyleBackColor = true;
+            this.normalConvert.Click += new System.EventHandler(this.normalConvert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 761);
+            this.Controls.Add(this.normalConvert);
+            this.Controls.Add(this.pointer);
+            this.Controls.Add(this.memory);
+            this.Controls.Add(this.pixel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Open);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button Close;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button pixel;
+        private System.Windows.Forms.Button memory;
+        private System.Windows.Forms.Button pointer;
+        private System.Windows.Forms.Button normalConvert;
+    }
+}
+
---
+++ ImageProcess_/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ImageProcess_
+{
+    public partial class Form1 : Form
+    {
+        private string curFileName;
+        private Bitmap curBitmap;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opnDiag = new OpenFileDialog();
+            //opnDiag.Filter = "All Image| *.bmp; *.png; *.gif; *.jpg; " + "*.tif; *.ico";
+            opnDiag.Filter = "All Image Files|*.bmp;*.ico;*.gif;*.jpeg;*.jpg;*.png;*.tif;*.tiff|" +
+                             "Windows Bitmap(*.bmp)|*.bmp|" +
+                             "Windows Icon(*.ico)|*.ico|" +
+                             "Graphics Interchange Format (*.gif)|(*.gif)|" +
+                             "JPEG File Interchange Format (*.jpg)|*.jpg;*.jpeg|" +
+                             "Portable Network Graphics (*.png)|*.png|" +
+                             "Tag Image File Format (*.tif)|*.tif;*.tiff";
+            opnDiag.Title = "Open Image File";
+            opnDiag.ShowHelp = true;
+            if (opnDiag.ShowDialog() == DialogResult.OK)
+            {
+                curFileName = opnDiag.FileName;
+                try
+                {
+                    curBitmap = (Bitmap)Image.FromFile(curFileName);
+                    //MessageBox.Show(curFileName);
+                    label1.Text += curFileName;
+                    this.Text = curFileName;
+                }
+                catch (Exception exp)
+                {
+                    MessageBox.Show(exp.Message + curFileName);
+                }
+            }
+            Invalidate();
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            if (curBitmap == null)
+                return;
+            SaveFileDialog saveDiag = new SaveFileDialog();
+            saveDiag.Title = "Save as";
+            saveDiag.OverwritePrompt = true;
+            // saveDiag.Filter = "bmp file(*.bmp)|*.bmp|" + "Gif file(*.gif)|*.gif|" + "Jpeg file(*.jpg)|*.jpg|" + "Png file(*.png)|*.png|";
+            saveDiag.Filter = "All Image Files|*.bmp;*.ico;*.gif;*.jpeg;*.jpg;*.png;*.tif;*.tiff|" +
+                             "Windows Bitmap(*.bmp)|*.bmp|" +
+                             "Windows Icon(*.ico)|*.ico|" +
+                             "Graphics Interchange Format (*.gif)|(*.gif)|" +
+                             "JPEG File Interchange Format (*.jpg)|*.jpg;*.jpeg|" +
+                             "Portable Network Graphics (*.png)|*.png|" +
+                             "Tag Image File Format (*.tif)|*.tif;*.tiff";
+            saveDiag.ShowHelp = true;
+            if (saveDiag.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = saveDiag.FileName;
+                string strFilExtn = fileName.Remove(0, fileName.Length - 3);
+                switch (strFilExtn)
+                {
+                    case "bmp":
+                        curBitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                        break;
+                    case "gif":
+                        curBitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Gif);
+                        break;
+                    case "jpg":
+                        curBitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+                    case "png":
+                        curBitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Png);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            if (curBitmap != null)
+            {
+                g.DrawImage(curBitmap, 160, 50, curBitmap.Width, curBitmap.Height);
+            }
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pixel_Click(object sender, EventArgs e)
+        {
+            if (curBitmap != null)
+            {
+                Color curColor;
+                int ret;
+                for (int i = 0; i < curBitmap.Width; i++)
+                {
+                    for (int j = 0; j < curBitmap.Height; j++)
+                    {
+                        curColor = curBitmap.GetPixel(i, j);
+                        ret = (int)(curColor.R * 0.299 + curColor.G * 0.587 + curColor.B * 0.114);
+                        curBitmap.SetPixel(i, j, Color.FromArgb(ret, ret, ret));
+                    }
+                }
+                Invalidate();
+            }
+        }
+
+        private void memory_Click(object sender, EventArgs e)
+        {
+            
+            //这里因为图像的尺寸是512x512，所以不用考虑未使用的空间
+            if (curBitmap != null)
+            {
+                Rectangle rect = new Rectangle(0, 0, curBitmap.Width, curBitmap.Height);
+                //注意，PixelFormat返回图像的像素格式，颜色值顺序是蓝、绿、红
+                System.Drawing.Imaging.BitmapData bmpData = curBitmap.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite, curBitmap.PixelFormat);
+                IntPtr ptr = bmpData.Scan0;
+                int bytes = curBitmap.Width * curBitmap.Height * 3;
+                byte[] rgbValues = new byte[bytes];
+                System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+                double colorTemp = 0;
+                for (int i = 0; i < rgbValues.Length; i += 3)
+                {
+                    colorTemp = rgbValues[i + 2] * 0.299 + rgbValues[i + 1] * 0.587 + rgbValues[i] * 0.114;
+                    rgbValues[i] = rgbValues[i + 1] = rgbValues[i + 2] = (byte)colorTemp;
+                }
+                System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+                curBitmap.UnlockBits(bmpData);
+                Invalidate();
+            }
+
+
+            if (curBitmap != null)
+            {
+                 
+            }
+            
+        }
+
+
+
+        private void pointer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void normalConvert_Click(object sender, EventArgs e)
+        {
+
+            if (curBitmap != null)
+            {
+                Rectangle rect = new Rectangle(0, 0, curBitmap.Width, curBitmap.Height);
+                //注意，PixelFormat返回图像的像素格式，颜色值顺序是红、绿、蓝
+                System.Drawing.Imaging.BitmapData bmpData = curBitmap.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite, curBitmap.PixelFormat);
+                IntPtr ptr = bmpData.Scan0;
+                int bytes = curBitmap.Width * curBitmap.Height * 3;
+                byte[] rgbValues = new byte[bytes];
+                System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+                double colorTemp = 0;
+                for (int i = 0; i < rgbValues.Length; i += 3)
+                {
+                    colorTemp = rgbValues[i + 2] * 0.299 + rgbValues[i + 1] * 0.587 + rgbValues[i] * 0.114;
+                    rgbValues[i] = rgbValues[i + 1] = rgbValues[i + 2] = (byte)colorTemp;
+                }
+
+                System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+                curBitmap.UnlockBits(bmpData);
+                Invalidate();
+            }
+
+        }
+    }
+}
